62d1836744cbb8adc3ac02875568bf3941c73d8e
truemaker <thorreyer@gmail.com>
2022-08-10 13:50:52 +0200

    failing to go to user mode

--- kernel/ring3/umode.asm
+++ kernel/ring3/umode.asm
@@ -1,20 +1,17 @@
-; note: code is untested
 global jump_usermode
 extern test_user_function
 jump_usermode:
-	mov ax, (4 * 8) | 3 ; user data segment with RPL 3
+	mov ax, (4 * 8) | 3 ; ring 3 data with bottom 2 bits set for ring 3
 	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax ; sysexit sets SS
+	mov es, ax 
+	mov fs, ax 
+	mov gs, ax ; SS is handled by iret
  
-	; setup wrmsr inputs
-	xor edx, edx ; not necessary; set to 0
-	mov eax, 0x100008 ; SS=0x10+0x10=0x20, CS=0x8+0x10=0x18
-	mov ecx, 0x174 ; MSR specifier: IA32_SYSENTER_CS
-	wrmsr ; set sysexit segments
- 
-	; setup sysexit inputs
-	mov edx, test_user_function ; to be loaded into EIP
-	mov ecx, esp ; to be loaded into ESP
-	sysexit
+	; set up the stack frame iret expects
+	mov eax, esp
+	push (4 * 8) | 3 ; data selector
+	push eax ; current esp
+	pushf ; eflags
+	push (3 * 8) | 3 ; code selector (ring 3 code with bottom 2 bits set for ring 3)
+	push test_user_function ; instruction address to return to
+	iret
