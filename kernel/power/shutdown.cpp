74f2b9f58d8e7e74eaf3ada009c576781404535c
truemaker <thorreyer@gmail.com>
2022-08-30 19:21:05 +0200

    Compact

--- kernel/power/shutdown.cpp
+++ kernel/power/shutdown.cpp
@@ -8,38 +8,18 @@ void power_shutdown() {
     asm("hlt");
 }
 
-/* keyboard interface IO port: data and control
-   READ:   status port
-   WRITE:  control register */
-#define KBRD_INTRFC 0x64
- 
-/* keyboard interface bits */
-#define KBRD_BIT_KDATA 0 /* keyboard data is in buffer (output buffer is empty) (bit 0) */
-#define KBRD_BIT_UDATA 1 /* user data is in buffer (command buffer is empty) (bit 1) */
- 
-#define KBRD_IO 0x60 /* keyboard IO port */
-#define KBRD_RESET 0xFE /* reset CPU command */
- 
-#define bit(n) (1<<(n)) /* Set bit n to 1 */
- 
-/* Check if bit n in flags is set */
-#define check_flag(flags, n) ((flags) & bit(n))
-
 void power_restart() {
     uint8_t temp;
- 
-    asm ("cli"); /* disable all interrupts */
- 
-    /* Clear all keyboard buffers (output and command buffers) */
+    asm ("cli");
     do
     {
-        temp = inb(KBRD_INTRFC); /* empty user data */
-        if (check_flag(temp, KBRD_BIT_KDATA) != 0)
-            inb(KBRD_IO); /* empty keyboard data */
-    } while (check_flag(temp, KBRD_BIT_UDATA) != 0);
+        temp = inb(0x64);
+        if (((temp) & (1)) != 0)
+            inb(0x60);
+    } while (((temp) & (1<<1)) != 0);
  
-    outb(KBRD_INTRFC, KBRD_RESET); /* pulse CPU reset line */
-loop:
-    asm ("hlt"); /* if that didn't work, halt the CPU */
-    goto loop; /* if a NMI is received, halt again */
+    outb(0x64, 0xFE);
+    while (1) {
+        asm("hlt");
+    }
 }
