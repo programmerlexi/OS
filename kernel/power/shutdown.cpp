515fb5ed09670992dfb6454166d0df02ef651a35
truemaker <thorreyer@gmail.com>
2022-08-30 19:27:44 +0200

    Commented code

--- kernel/power/shutdown.cpp
+++ kernel/power/shutdown.cpp
@@ -13,13 +13,13 @@ void power_restart() {
     asm ("cli");
     do
     {
-        temp = inb(0x64);
-        if (((temp) & (1)) != 0)
-            inb(0x60);
-    } while (((temp) & (1<<1)) != 0);
+        temp = inb(0x64); // Read from the Keyboard Interface IO port to clear the user data
+        if (((temp) & (1)) != 0) // Check if the keyboard data buffer is clear
+            inb(0x60); // Read from the keyboard IO port to clear the user data
+    } while (((temp) & (1<<1)) != 0); // Repeat until both buffers are clear
  
-    outb(0x64, 0xFE);
-    while (1) {
+    outb(0x64, 0xFE); // Write CPU_RESET(0xfe) to the keyboard interface IO port
+    while (1) { // If the above code does not work, halt the cpu.
         asm("hlt");
     }
 }
